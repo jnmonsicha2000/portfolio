206f800f7fa64ba548641fe7293d7a376608a422
jnmonsicha2000 <94210109+jnmonsicha2000@users.noreply.github.com>
2023-11-04 13:30:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function scrollToElement(elementSelector, instance =0){
+    const elements = document.querySelectorAll(elementSelector);
+
+    if(elements.length > instance) {
+
+        elements[instance].scrollIntoView({ behavior: 'smooth'});
+    }
+}
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+const link4 = document.getElementById("link4");
+
+link1.addEventListener('click', () =>{
+    scrollToElement('.hero-con');
+});
+link2.addEventListener('click', () =>{
+    scrollToElement('.header');
+});
+link3.addEventListener('click', () =>{
+    scrollToElement('.column');
+});
+link4.addEventListener('click', () =>{
+    scrollToElement('.contact-text');
+});
